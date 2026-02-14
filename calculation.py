b86fd7130445d1df3a262e8b1e871b1aa93dbd69
9354077723ds-cloud <9354077723ds@gmail.com>
2026-02-14 13:32:58 +0530

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,13 @@
+f = int(input("enter the first number: "))
+s = int(input("enter the second number: "))
+
+Ad = f+s
+sub = f-s
+mul = f*s
+div = f//s
+
+print("Addition: ",Ad)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
+
